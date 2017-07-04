69fb62fb341b9009d8238689cad18850676344e3
Philip Larsson <elt11pla@student.lu.se>
2017-07-04 21:54:54 +0200

    Reformat routes for api
    Move route into group for better readability.

--- routes/api.php
+++ routes/api.php
@@ -5,8 +5,9 @@ use App\Controllers\CookieController;
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
-$app->group('/api/v1/week', function () {
-    $this->get('/{week}', CookieController::class . ':getWeekCookie')->setName('get-week');
+$app->group('/api/v1', function () {
+    $this->get('/thisweek', CookieController::class . ':getThisWeek')->setName('get-this-week');
+    $this->get('/week/{week}', CookieController::class . ':getWeekCookie')->setName('get-week');
 });
 
-$app->get('/api/v1/thisweek', CookieController::class . ':getThisWeek')->setName('get-this-week');
+
